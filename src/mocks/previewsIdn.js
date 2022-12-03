77e556630f7d9a3c3b75ef69fb08fba804725286
Nikita <schkabura2014@ya.ru>
2022-12-03 22:00:51 +0400

    fix

---
+++ src/mocks/previewsIdn.js
@@ -0,0 +1,329 @@
+import imgOne from '../assets/imgs/preview-300.png'
+import imgTwo from '../assets/imgs/preview-500.png'
+import imgThree from '../assets/imgs/preview-900.png'
+import imgFour from '../assets/imgs/preview-1100.png'
+import imgFive from '../assets/imgs/preview-dampers.png'
+
+import idn300_1 from '../assets/imgs/idn300-1.png'
+import idn300_2 from '../assets/imgs/idn300-2.png'
+import idn300_3 from '../assets/imgs/idn300-3.png'
+import idn300_4 from '../assets/imgs/idn300-4.png'
+import idn300_5 from '../assets/imgs/idn300-5.png'
+
+import idn500_1 from '../assets/imgs/idn500-1.png'
+import idn500_2 from '../assets/imgs/idn500-2.png'
+import idn500_3 from '../assets/imgs/idn500-3.png'
+import idn500_4 from '../assets/imgs/idn500-4.png'
+import idn500_5 from '../assets/imgs/idn500-5.png'
+import idn500_6 from '../assets/imgs/idn500-6.png'
+
+import idn900_1 from '../assets/imgs/idn900-1.png'
+import idn900_2 from '../assets/imgs/idn900-2.png'
+import idn900_3 from '../assets/imgs/idn900-3.png'
+import idn900_4 from '../assets/imgs/idn900-4.png'
+import idn900_5 from '../assets/imgs/idn900-5.png'
+
+import idn1100_1 from '../assets/imgs/idn1100-1.png'
+import idn1100_2 from '../assets/imgs/idn1100-2.png'
+import idn1100_3 from '../assets/imgs/idn1100-3.png'
+
+import dampers_1 from '../assets/imgs/dampers-1.png'
+import dampers_2 from '../assets/imgs/dampers-2.png'
+import dampers_3 from '../assets/imgs/dampers-3.png'
+import dampers_4 from '../assets/imgs/dampers-4.png'
+import dampers_5 from '../assets/imgs/dampers-5.png'
+import dampers_6 from '../assets/imgs/dampers-6.png'
+import dampers_7 from '../assets/imgs/dampers-7.png'
+import dampers_8 from '../assets/imgs/dampers-8.png'
+import dampers_9 from '../assets/imgs/dampers-9.png'
+import dampers_10 from '../assets/imgs/dampers-10.png'
+
+export const previewsIdn = [
+    {
+        id: 1,
+        url: imgOne,
+        title: 'ИДН 300',
+        description: 'ИДН 300 от производителя с доставкой по всей России',
+        products: [
+            {
+                url: idn300_1,
+                title: 'ИДН300 средний композитный',
+                size: 'Д300 х Ш500 х В50',
+                weight: '8,3',
+                material: 'Композит',
+                ankers: '4',
+                price: '850',
+            },
+            {
+                url: idn300_2,
+                title: 'ИДН300 средний желтый',
+                size: 'Д300 х Ш500 х В50',
+                weight: '8,3',
+                material: 'Композит',
+                ankers: '4',
+                price: '900',
+            },
+            {
+                url: idn300_3,
+                title: 'ИДН300 концевой резина',
+                size: 'Д300 x Ш180 х В37',
+                weight: '1,5',
+                material: 'Резина',
+                ankers: '2',
+                price: '400',
+            },
+            {
+                url: idn300_4,
+                title: 'ИДН300 концевой композитный',
+                size: 'Д300 x Ш250 х В50',
+                weight: '3',
+                material: 'Композит',
+                ankers: '3',
+                price: '450',
+            },
+            {
+                url: idn300_5,
+                title: 'ИДН300 средний резина',
+                size: 'Д300 x Ш500 х В37',
+                weight: '5,8',
+                material: 'Резина',
+                ankers: '4',
+                price: '850',
+            },
+        ]
+    },
+    {
+        id: 2,
+        url: imgTwo,
+        title: 'ИДН 500',
+        description: 'ИДН 500 от производителя с доставкой по всей России',
+        products: [
+            {
+                url: idn500_1,
+                title: 'ИДН500 средний композитный',
+                size: 'Д500 x Ш500 х В60',
+                weight: '15,8',
+                material: 'Композит',
+                ankers: '4',
+                price: '1 100',
+            },
+            {
+                url: idn500_2,
+                title: 'ИДН500 средний желтый',
+                size: 'Д500 x Ш500 х В60',
+                weight: '15,8',
+                material: 'Композит',
+                ankers: '4',
+                price: '1 200',
+            },
+            {
+                url: idn500_3,
+                title: 'ИДН500 концевой композитный',
+                size: 'Д500 x Ш250 х В60',
+                weight: '6',
+                material: 'Композит',
+                ankers: '2',
+                price: '550',
+            },
+            {
+                url: idn500_4,
+                title: 'ИДН500 средний с кабель каналом',
+                size: 'Д500 x Ш500 х В58',
+                weight: '16,5',
+                material: 'Композит',
+                ankers: '4',
+                price: '1200',
+            },
+            {
+                url: idn500_5,
+                title: 'ИДН500 средний резина',
+                size: 'Д500 x Ш500 х В58',
+                weight: '14',
+                material: 'Резина',
+                ankers: '6',
+                price: '1200',
+            },
+            {
+                url: idn500_6,
+                title: 'ИДН500 концевой резина',
+                size: 'Д500 x Ш250 х В58',
+                weight: '14,6',
+                material: 'Резина',
+                ankers: '4',
+                price: '600',
+            },
+        ],
+    },
+    {
+        id: 3,
+        url: imgThree,
+        title: 'ИДН 900',
+        description: 'ИДН 900 от производителя с доставкой по всей России',
+        products: [
+            {
+                url: idn900_1,
+                title: 'ИДН900 средний композитный',
+                size: 'Д900 x Ш500 х В60',
+                weight: '29',
+                material: 'Композит',
+                ankers: '10',
+                price: '2 200',
+            },
+            {
+                url: idn900_2,
+                title: 'ИДН900 средний желтый',
+                size: 'Д900 x Ш500 х В60',
+                weight: '29',
+                material: 'Композит',
+                ankers: '10',
+                price: '2 350',
+            },
+            {
+                url: idn900_3,
+                title: 'ИДН900 концевой композит',
+                size: 'Д900 x Ш250 х В60',
+                weight: '10,5',
+                material: 'Полимерный композит',
+                ankers: '4',
+                price: '1 100',
+            },
+            {
+                url: idn900_4,
+                title: 'ИДН900 средний резина',
+                size: 'Д900 x Ш500 х В58',
+                weight: '28',
+                material: 'Резина',
+                ankers: '12',
+                price: '2 400',
+            },
+            {
+                url: idn900_5,
+                title: 'ИДН900 концевой резина',
+                size: 'Д500 x Ш500 х В58',
+                weight: '6,75',
+                material: 'Резина',
+                ankers: '4',
+                price: '1 200',
+            },
+        ],
+    },
+    {
+        id: 4,
+        url: imgFour,
+        title: 'ИДН 1100',
+        description: 'ИДН 1100 от производителя с доставкой по всей России',
+        products: [
+            {
+                url: idn1100_1,
+                title: 'ИДН1100 средний композитный из 2 частей',
+                size: 'Д1100 x Ш500 х В60',
+                weight: '36',
+                material: 'Композит',
+                ankers: '10',
+                price: '4 500',
+            },
+            {
+                url: idn1100_2,
+                title: 'ИДН1100 средний желтый из 2 частей',
+                size: 'Д1100 x Ш500 х В60',
+                weight: '36',
+                material: 'Композит',
+                ankers: '10',
+                price: '4 900',
+            },
+            {
+                url: idn1100_3,
+                title: 'ИДН1100 концевой композитный из 2 частей',
+                size: 'Д1100 x Ш500 х В60',
+                weight: '32',
+                material: 'Композит',
+                ankers: '6',
+                price: '4 200',
+            },
+        ],
+    },
+    {
+        id: 5,
+        url: imgFive,
+        title: 'Демпферы, съезды с бордюра, колесоотбойники',
+        description: 'От производителя с доставкой по всей России',
+        products: [
+            {
+                url: dampers_1,
+                title: 'Съезд с бордюра средний элемент',
+                size: 'Д250 x Ш500 х В58',
+                weight: '8,3',
+                ankers: '2',
+                price: '850',
+            },
+            {
+                url: dampers_2,
+                title: 'Съезд с бордюра концевой элемент',
+                size: 'Д250 x Ш250 х В58',
+                weight: '3',
+                ankers: '1',
+                price: '600',
+            },
+            {
+                url: dampers_3,
+                title: 'КО915 комплект из 2 частей',
+                size: 'Д1830 x Ш150 х В100',
+                weight: '24',
+                ankers: '4',
+                price: '1 900',
+            },
+            {
+                url: dampers_4,
+                title: 'КО915 серый комплект из 2 частей',
+                size: 'Д1830 x Ш150 х В100',
+                weight: '24',
+                ankers: '4',
+                price: '1 900',
+            },
+            {
+                url: dampers_5,
+                title: 'КО500',
+                size: 'Д500 x Ш115 х В100',
+                weight: '4,5',
+                ankers: '2',
+                price: '550',
+            },
+            {
+                url: dampers_6,
+                title: 'Колесоотбойник КО-600 для парковки',
+                size: 'Д600 x Ш115 х В100',
+                weight: '5,5',
+                ankers: '2',
+                price: '600',
+            },
+            {
+                url: dampers_7,
+                title: 'Демпферы угловые ДУ800 отражатели желтый, белый',
+                size: 'Д800 x Ш95 х В10',
+                weight: '2,5',
+                price: '550',
+            },
+            {
+                url: dampers_8,
+                title: 'Демпфер угловой ДУ900 отражатели желтый, белый',
+                size: 'Д900 x Ш95 х В10',
+                weight: '3',
+                price: '600',
+            },
+            {
+                url: dampers_9,
+                title: 'Демпфер стеновой ДС1000П с прямоугольным отражателем желтый, белый',
+                size: 'Д1000 x Ш95 х В10',
+                weight: '2,5',
+                price: '650',
+            },
+            {
+                url: dampers_10,
+                title: 'Демпфер стеновой ДС1000С с отражателем "стрелка" желтый, белый',
+                size: 'Д1000 x Ш95 х В10',
+                weight: '2,5',
+                price: '650',
+            },
+        ],
+    },
+]
